2ccb932de6eca00171fdf790171eb90dde0cfeff
Guillermo Verón <guiver95@gmail.com>
2022-08-15 17:36:28 -0400

    error handling

---
+++ pages/event/_id.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>Event #{{ this.id }}</h1>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Event #' + this.id,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'What you need to know about Event #' + this.id,
+        },
+      ],
+    }
+  },
+  computed: {
+    id() {
+      return this.$route.params.id
+    },
+  },
+}
+</script>
+
+<style></style>
